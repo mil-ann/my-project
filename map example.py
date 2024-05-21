800a98776ca671a140d0a50316d74d95565a82e0
mil-ann <milanbishankhe77@gmail.com>
2024-05-21 17:11:11 +0545

    map example

--- map example.py
+++ map example.py
@@ -1,7 +1,9 @@
 def capitalize_and_ascii_sum(word:str):
+    return sum(ord(x) for x in word.capitalize())
+
 animals = ['cat','dog','cow']
 animals_output = []
 
-animals_output = list(map(capitalize_and_ascii_sum, animals))
-print(animals_output)
+animal_output = list(map(capitalize_and_ascii_sum,animals))
+print(animal_output)
 
